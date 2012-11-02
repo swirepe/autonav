c5150c60e1b51d5a1a1a65e8b5549cd137c6e45f
Peter Swire <pswire@cargometrics.com>
2012-11-02 17:47:57 -0400

    set it up for my work computer and made a viewnavs function that shows what was created by this script

--- autonav.py
+++ autonav.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-
 import ConfigParser
 import glob
 import os
 import sys
 import tempfile
+
+from collections import defaultdict
 
 def getPaths():
     conf = sys.argv[1:]    
@@ -33,25 +34,81 @@ def getPaths():
     return items
     
 
+def pathsAndNames(paths):
+    """
+    if two top-level names are the same, disambiguate them
+    by putting in a hyphen
+    """
+    dupes = defaultdict(list)
+    
+    for path in paths:
+        dupes[ os.path.basename(path) ].append(path)
+        
+    pairs = []
+    for base, path in dupes.iteritems():
+        if len(path) > 1:
+            for p in path:
+                prefix = os.path.basename(os.path.dirname(p))
+                pairs.append( (p, prefix + "-" + base) )
+        else:
+            pairs.append( (path[0], base) )
+
+    return pairs
+    
+
+def lowercasePathsAndNames(pathPairs):
+    """doubles the length of this by turning things into lowercase also"""
+    newPairs = pathPairs[:]
+    
+    for path, base in pathPairs:
+        if base.lower() != base:
+            newPairs.append( (path, base.lower() ) )
+                
+    #print newPairs
+    return newPairs
 
 
-def writeFn(path, f):
+def writeFn(path, name, f):
     strtemplate = """
     
 function %s {
     cd %s
 }
     
-""" % ( os.path.basename(path), path)
+""" % ( name, path)
     
     f.write(strtemplate)
 
+
+
+def writeMappings(pathPairs, f):
+    pathmax = max([len(path) for (path, name) in pathPairs])
+    namemax = max([len(name) for (path, name) in pathPairs])
+    
+    # put things in order, plz
+    pathPairs.sort(key=lambda tup: tup[1])
+    
+    # start the function
+    f.write("function viewnavs {\n\n" )
+    
+    for path, name in pathPairs:
+        f.write('    echo "%s\t%s"\n' % (name.ljust(namemax), path.rjust(pathmax)) )
+        
+    f.write("\n\n}\n")
+    
+    
 
 
 if __name__ == "__main__":
     paths = getPaths()
     f = tempfile.NamedTemporaryFile(delete=False)
 
-    map(lambda path: writeFn(path, f), paths)
+    pathPairs = lowercasePathsAndNames(pathsAndNames(paths))
 
+    for path, name in pathPairs:
+        writeFn(path, name, f)
+
+    writeMappings(pathPairs, f)
+    
     print f.name
+    f.close()
